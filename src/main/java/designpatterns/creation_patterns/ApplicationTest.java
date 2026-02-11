252f3bc606311bc856513be899da85a33058e72a
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2026-02-11 17:21:15 +0100

    last commit in Builder pattern

--- src/main/java/designpatterns/creation_patterns/ApplicationTest.java
+++ src/main/java/designpatterns/creation_patterns/ApplicationTest.java
@@ -1,8 +1,11 @@
 package designpatterns.creation_patterns;
 
+import designpatterns.creation_patterns.model.AccountType;
 import designpatterns.creation_patterns.model.BankAccount;
 import designpatterns.creation_patterns.repository.BankAccountRepositoryImpl;
 import designpatterns.utils.JsonSerializer;
+
+import java.util.function.Predicate;
 
 public class ApplicationTest {
     public static void main(String[] args) {
@@ -11,5 +14,15 @@ public class ApplicationTest {
         accountRepository.manipulateData();
         accountRepository.findAll().stream().map(bankAccountJsonSerializer::toJson)
                 .forEach(System.out::println);
+        System.out.println("----------- Call search method -----------");
+        accountRepository.searchAccount(new Predicate<BankAccount>() {
+                    @Override
+                    public boolean test(BankAccount account) {
+                        return account.getType().equals(AccountType.SAVING_ACCOUNT);
+                    }
+                })
+                .stream().map(bankAccountJsonSerializer::toJson)
+                .forEach(System.out::println);
+
     }
 }
